3a044671b75a99878285efd5252a6b1f54618bff
Mr.Buzz <arnjain3@gmail.com>
2019-03-19 06:34:38 +0530

    V1.02 > replace if with fuction

--- realdaycounting.py
+++ realdaycounting.py
@@ -3,6 +3,25 @@
 # A leap year is 6 Hours in 1 year
 from math import *
 
+def realdays(days):
+	print("How many Days?")
+	noy = days / 365				#days into years
+	leapdays = noy * 0.25			#how many leap days
+	totalnod = leapdays + days		#days + leap days = total days
+	mdays = totalnod - days 		#diffrence in days
+	lnoy = days / 365.25			#number of years in accounting leap years
+	nooy = totalnod / 365 			#number of only years
+	nom = ((nooy % 1) * 12)			#number of months
+	nod = ((nom % 1) * 30)			#number of days
+	noh = ((nod % 1) * 24)			#number of hours
+	nomin = ((nod % 1)* 60)			#number of minutes
+	print("Number of days : ", days)
+	print("Number of days accounting for leap year : ", round(totalnod,2))
+	print("Number of more days :", round(mdays,4))
+	print("Actual time of",round(days,-1), "days :" , round(nooy,0), "year &", round(nom,0), "months &", round(nod,0), "days &", round(noh,0), "hours &", round(nomin,1), "minutes")
+
+
+'''
 print ("How many Days/Years?(D for Days/Y for Years)")
 selection = input()
 
@@ -45,3 +64,4 @@ if selection == "y":
 
 print("Note: This is not fully accurate as the days of the months are not taken in consideration")
 #	1 year = 12 months = 52 weeks = 365.25 days
+'''
