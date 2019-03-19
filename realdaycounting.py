a31f83c6c9a4df7b6d8a911e8714fcfaaa08e62f
Mr.Buzz <arnjain3@gmail.com>
2019-03-19 09:11:19 +0530

    V2.0 > just works

--- realdaycounting.py
+++ realdaycounting.py
@@ -1,34 +1,40 @@
 #!/usr/bin/env python
-
-
 print("You want to enter the Data in days or years?")
 print("(type d for days and y for years)")
+dory = input()
 print("format = realdays(d or y, the days or years)")
 
 
-def realdays(dory,data):
-	if dory == "d":
-		years =  data / 365				#days into years
-		leapdays = years * 0.25			#how many leap days
-		totalnod = leapdays + data		#days + leap days = total days
-		mdays = totalnod - data	 		#diffrence in days
-		lnoy = data / 365.25			#number of years in accounting leap years
-		nooy = totalnod / 365 			#number of only years
-		nom = ((nooy % 1) * 12)			#number of months
-		nod = ((nom % 1) * 30)			#number of days
-		noh = ((nod % 1) * 24)			#number of hours
-		nomin = ((nod % 1)* 60)			#number of minutes
-		print("Number of days : ", data)
-	elif dory == "y":
-		nod = data * 365
-		leapdays = data * 0.25
-		totalnod = nod + leapdays
-		mdays = totalnod - nod
-		lnoy = totalnod / 365
-		print("Number of years: ", data)
+def realdays(data):
+    if dory == "days":
+        years = data / 365  # days into years
+        leapdays = years * 0.25  # how many leap days
+        totalnod = leapdays + data  # days + leap days = total days
+        mdays = totalnod - data  # diffrence in days
+        lnoy = data / 365.25  # number of years in accounting leap years
+        nooy = totalnod / 365  # number of only years
+        nom = ((nooy % 1) * 12)  # number of months
+        nod = ((nom % 1) * 30)  # number of days
+        noh = ((nod % 1) * 24)  # number of hours
+        nomin = ((nod % 1) * 60)  # number of minutes
+        print("Number of days : ", data)
+    elif dory == "years":
+        nod = data * 365
+        leapdays = data * 0.25
+        totalnod = nod + leapdays
+        mdays = totalnod - nod
+        lnoy = totalnod / 365
+        lnoy = data / 365.25
+        nooy = totalnod / 365
+        nom = ((nooy % 1) * 12)
+        nod = ((nom % 1) * 30)
+        noh = ((nod % 1) * 24)
+        nomin = ((nod % 1) * 60)
+        print("Number of years: ", data)
+    print("Number of days accounting for leap year : ", round(totalnod, 2))
+    print("Number of more days :", round(mdays, 2))
+    print("Number of years accounting for leap year : ", round(lnoy, 2))
+    print("Actual time of", round(data, 0), "days :", round(nooy, 0), "year &", round(nom, 0),
+          "months &", round(nod, 0), "days &", round(noh, 0), "hours &", round(nomin, 1), "minutes")
 
-print("Number of days accounting for leap year : ", round(totalnod,2))
-print("Number of more days :", round(mdays,2))
-print("Number of years accounting for leap year : ", round(lnoy,2))
-
-print("Note: This is not fully accurate as the days of the months are not taken in consideration")
+#print("Note: This is not fully accurate as the days of the months are not taken in consideration")
