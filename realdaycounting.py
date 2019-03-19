e3663c46cd46a61735414b5209d58f7f025d9e7d
Mr.Buzz <arnjain3@gmail.com>
2019-03-19 08:41:37 +0530

    V1.04 > cleaner, better but errors

--- realdaycounting.py
+++ realdaycounting.py
@@ -1,65 +1,34 @@
 #!/usr/bin/env python
 
-def realdays(x):
-	noy = x / 365					#days into years
-	leapdays = noy * 0.25			#how many leap days
-	totalnod = leapdays + x			#days + leap days = total days
-	mdays = totalnod - x	 		#diffrence in days
-	lnoy = x / 365.25				#number of years in accounting leap years
-	nooy = totalnod / 365 			#number of only years
-	nom = ((nooy % 1) * 12)			#number of months
-	nod = ((nom % 1) * 30)			#number of days
-	noh = ((nod % 1) * 24)			#number of hours
-	nomin = ((nod % 1)* 60)			#number of minutes
+
+print("You want to enter the Data in days or years?")
+print("(type d for days and y for years)")
+print("format = realdays(d or y, the days or years)")
 
 
+def realdays(dory,data):
+	if dory == "d":
+		years =  data / 365				#days into years
+		leapdays = years * 0.25			#how many leap days
+		totalnod = leapdays + data		#days + leap days = total days
+		mdays = totalnod - data	 		#diffrence in days
+		lnoy = data / 365.25			#number of years in accounting leap years
+		nooy = totalnod / 365 			#number of only years
+		nom = ((nooy % 1) * 12)			#number of months
+		nod = ((nom % 1) * 30)			#number of days
+		noh = ((nod % 1) * 24)			#number of hours
+		nomin = ((nod % 1)* 60)			#number of minutes
+		print("Number of days : ", data)
+	elif dory == "y":
+		nod = data * 365
+		leapdays = data * 0.25
+		totalnod = nod + leapdays
+		mdays = totalnod - nod
+		lnoy = totalnod / 365
+		print("Number of years: ", data)
 
-	print("Number of days : ", x)
-	print("Number of days accounting for leap year : ", round(totalnod,2))
-	print("Number of more days :", round(mdays,4))
-	print("Actual time of",round(x,0), "days :" , round(nooy,0), "year &", round(nom,0), "months &", round(nod,0), "days &", round(noh,0), "hours &", round(nomin,1), "minutes")
-
-
-'''
-print ("How many Days/Years?(D for Days/Y for Years)")
-selection = input()
-
-if selection == "d":
-	print("How many Days?")
-	days = float(input())
-	noy = days / 365				#days into years
-	leapdays = noy * 0.25			#how many leap days
-	totalnod = leapdays + days		#days + leap days = total days
-	mdays = totalnod - days 		#diffrence in days
-	lnoy = days / 365.25			#number of years in accounting leap years
-	nooy = totalnod / 365 			#number of only years
-	nom = ((nooy % 1) * 12)			#number of months
-	nod = ((nom % 1) * 30)			#number of days
-	noh = ((nod % 1) * 24)			#number of hours
-	nomin = ((nod % 1)* 60)			#number of minutes
-	#----------Print-----------
-	print("Number of days : ", days)
-	print("Number of days accounting for leap year : ", round(totalnod,2))
-	print("Number of more days :", round(mdays,4))
-	print("Actual time of",round(days,-1), "days :" , round(nooy,0), "year &", round(nom,0), "months &", round(nod,0), "days &", round(noh,0), "hours &", round(nomin,1), "minutes")
-if selection == "y":
-	print("How many Years?")
-	years = float(input())
-	nod = years * 365
-	leapdays = years * 0.25
-	totalnod = nod + leapdays
-	mdays = totalnod - nod
-	lnoy = totalnod / 365
-	#----------Print-----------
-	print("Number of years: ", years)
-	print("Number of days : ", nod)
-	print("Number of days accounting for leap year : ", round(totalnod,2))
-	print("Number of more days :", round(mdays,2))
-	print("Number of years accounting for leap year : ", round(lnoy,2))
-
-
-
+print("Number of days accounting for leap year : ", round(totalnod,2))
+print("Number of more days :", round(mdays,2))
+print("Number of years accounting for leap year : ", round(lnoy,2))
 
 print("Note: This is not fully accurate as the days of the months are not taken in consideration")
-#	1 year = 12 months = 52 weeks = 365.25 days
-'''
