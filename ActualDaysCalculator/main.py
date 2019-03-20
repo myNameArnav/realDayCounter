67afbe815b241e924cf241852436f960678cf729
Mr.Buzz <arnjain3@gmail.com>
2019-03-20 23:57:52 +0530

    V2.01.04

--- ActualDaysCalculator/main.py
+++ ActualDaysCalculator/main.py
@@ -3,9 +3,16 @@ from realday import realdayscalc
 
 
 print("You want to enter the Data in days or years?")
-print("(type d for days and y for years)")
-dory = input()
-print("format = realdays(days or years, the days or years)")
-
-def opening():
-    exec(realdayscalc(data))
+dory == input()
+print ("how many?")
+data = input()
+'''
+if dory == "days":
+    print("How many days?")
+    data = input()
+    realdayscalc(data)
+if dory == "years":
+    print("How many years?")
+    data = input()
+    '''
+realdayscalc(data)
