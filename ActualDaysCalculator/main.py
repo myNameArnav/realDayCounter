8cbceff2a285a0609c58cf4a2ec8484d9448db5f
Mr.Buzz <arnjain3@gmail.com>
2019-03-20 20:51:22 +0530

    V2.01.2 > sorting things out

---
+++ ActualDaysCalculator/main.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+def main():
+    print("You want to enter the Data in days or years?")
+    print("(type d for days and y for years)")
+    dory = input()
+    print("format = realdays(days or years, the days or years)")
---
+++ ActualDaysCalculator/monthcalc.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+#how to add months and there days.
+
+from main import *
+
+
+def monthsfinder(month):
+    print(data)
