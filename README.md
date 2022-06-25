a7016a538ece8eb882e8f167891d2e68929bcf1b
Arnav Jain <arnjain3@gmail.com>
2022-06-25 23:42:56 +0530

    revamp

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
-<h1> RealDayCounter. </h1>
+# RealDayCounter
 
 Are the Days you count accurate?
 
-I got the idea while listening to 'The Fun in Life' by 'Elton Castee' at 0:38, he says "25,185 about how many days we have alive", from there I had a thought that did they include the leap year days or the six hours every year that turn out to be one day i.e. the 29th February.
+How to run
 
-What does it do:
+1. Clone Repo
+2. run 
+    - ```python main.py <day or year> <number of day or year>```
+    - Example ```python main.py day 2000```
+3. Use ```help``` for help
 
-So normally people just multiply the number of years with 365 to get the number of days but that's wrong they don't account leap years which does not make a whole lot of difference when there are a small number of days, but a large number the answer would differ slightly
-
-For Example, You take 20,000 days and multiply with 365 you'll get 73,00,000 but you don't account for the leap hours or the extra hours, that's what this small program will do.
-
-Just building on that I made this in a couple of days :)
+Also, if you want to know the thought behind this project you can read ...
---
+++ main.py
@@ -0,0 +1,51 @@
+import sys
+
+period = sys.argv[1]
+
+if period != "help":
+    time = float(sys.argv[2])
+
+forEveryHour = 6 / 365
+
+if period == "day":
+    days2Hour = time * 24
+    leapTime = round(((days2Hour + (days2Hour * forEveryHour)) / 24), 2)
+    print(
+        f"The actual days will be {time}, but adding the leap time it will be {leapTime}"
+    )
+    print(f"There will be a diffrence of {leapTime - time}")
+elif period == "year":
+    years2Hour = time * 365 * 24
+    leapTime = round(((years2Hour + (years2Hour * forEveryHour)) / (365 * 24)), 2)
+    print(
+        f"The actual years will be {time}, but adding the leap time it will be {leapTime}"
+    )
+    print(f"There will be a diffrence of {leapTime - time}")
+elif period == "plot":
+    import matplotlib.pyplot as plt
+
+    actualTime = []
+    leapTime = []
+    linear = []
+    diff = []
+    for i in range(int(round(time, 0))):
+        linear.append(i)
+        actualTime.append(float(i * (24)))
+        leapTime.append(round(((i * (24)) + (i * forEveryHour)), 2))
+        diff.append(round(((leapTime[i] - actualTime[i])), 2))
+    plt.plot(actualTime, linear, label="Actual Time")
+    plt.plot(leapTime, linear, label="Leap Time")
+    plt.plot(diff, linear, label="Difference")
+    plt.xlabel("Time")
+    plt.ylabel("Time")
+    plt.legend()
+    plt.show()
+elif period == "help":
+    print(
+        "------------\n"
+        "Options\n"
+        "\n"
+        "day <number>: for specifying the number of day\n"
+        "year <number>: for specifying number of years\n"
+        "help: you are here\n"
+    )
